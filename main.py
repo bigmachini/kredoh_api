8dec3cb6cd0ba108a6ef3c10b93b755fc1ca0459
bigmachini <elisha.wambiji@bigmachini.net>
2022-12-27 13:00:57 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from kredoh.routers import callback,kyanda,kredoh,mpesa
+from kredoh.routers import callback, kyanda, kredoh, mpesa
 
 app = FastAPI()
 app.include_router(callback.router)
 app.include_router(kyanda.router)
 app.include_router(kredoh.router)
 app.include_router(mpesa.router)
-
-
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=8010, reload=True)
